23559c1b020f7c0e62c70469b79af039337e6069
kjh0816 <readshot2@gmail.com>
2021-06-10 09:24:09 +0900

    DB.sql 수정 / 나를 1번 회원으로

--- db.sql
+++ db.sql
@@ -14,6 +14,48 @@ CREATE TABLE article (
     `body` TEXT NOT NULL
 );
 
+
+# 회원 테이블 생성
+CREATE TABLE `member` (
+    id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    regDate DATETIME NOT NULL,
+    updateDate DATETIME NOT NULL,
+    loginId CHAR(20) NOT NULL,
+    loginPw CHAR(100) NOT NULL,
+    `name` CHAR(20) NOT NULL,
+    nickname CHAR(20) NOT NULL,
+    email CHAR(100) NOT NULL,
+    cellphoneNo CHAR(20) NOT NULL
+);
+
+
+# 게시물 테이블에 작성자 정보 저장
+ALTER TABLE article ADD COLUMN memberId INT(10) UNSIGNED NOT NULL AFTER updateDate;
+
+# 기존 게시물을 특정 회원들과 연결, 랜덤으로
+UPDATE article
+SET memberId = id % 2 + 1
+WHERE memberId = 0;
+
+# 회원에 삭제여부 칼럼 추가
+ALTER TABLE `member` ADD COLUMN delStatus TINYINT(1) UNSIGNED NOT NULL DEFAULT 0 AFTER updateDate;
+ALTER TABLE `member` ADD COLUMN delDate DATETIME AFTER delStatus;
+
+
+# 1번 회원 생성
+
+INSERT INTO `member`
+SET regDate = NOW(),
+updateDate = NOW(),
+loginId = 'jihoo',
+loginPw = 'jihoo',
+`name` = 'jh',
+nickname = 'jh',
+email = 'readshot2@gmail.com',
+cellphoneNo = '01049219810';
+
+
+
 # 테스트 게시물 생성
 INSERT INTO article 
 SET regDate = NOW(),
@@ -39,29 +81,10 @@ updateDate = NOW(),
 title = '제목4',
 `body` = '내용4';
 
-# 회원 테이블 생성
-CREATE TABLE `member` (
-    id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    regDate DATETIME NOT NULL,
-    updateDate DATETIME NOT NULL,
-    loginId CHAR(20) NOT NULL,
-    loginPw CHAR(100) NOT NULL,
-    `name` CHAR(20) NOT NULL,
-    nickname CHAR(20) NOT NULL,
-    email CHAR(100) NOT NULL,
-    cellphoneNo CHAR(20) NOT NULL
-);
+
 
 # 테스트 회원 생성
-INSERT INTO `member`
-SET regDate = NOW(),
-updateDate = NOW(),
-loginId = 'jihoo',
-loginPw = 'jihoo',
-`name` = 'jh',
-nickname = 'jh',
-email = 'readshot2@gmail.com',
-cellphoneNo = '01049219810';
+
 
 INSERT INTO `member`
 SET regDate = NOW(),
@@ -73,17 +96,7 @@ nickname = '이또한',
 email = 'user2@test.com',
 cellphoneNo = '01022222222';
 
-# 게시물 테이블에 작성자 정보 저장
-ALTER TABLE article ADD COLUMN memberId INT(10) UNSIGNED NOT NULL AFTER updateDate;
 
-# 기존 게시물을 특정 회원들과 연결, 랜덤으로
-UPDATE article
-SET memberId = id % 2 + 1
-WHERE memberId = 0;
-
-# 회원에 삭제여부 칼럼 추가
-ALTER TABLE `member` ADD COLUMN delStatus TINYINT(1) UNSIGNED NOT NULL DEFAULT 0 AFTER updateDate;
-ALTER TABLE `member` ADD COLUMN delDate DATETIME AFTER delStatus;
 
 TRUNCATE article;
 
